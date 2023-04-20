1365f8fb5e58f3ab606a6f0c07e7c6ad8d912f68
Om.Parmar <Om.Parmar@theonetechnologies.co.in>
2023-04-20 10:59:50 +0530

    CreateFirm

--- apointify/Controllers/ServiceProviderController.cs
+++ apointify/Controllers/ServiceProviderController.cs
@@ -1,4 +1,6 @@
-﻿using apointify.Models;
+﻿using apointify.ExtentionMethods;
+using apointify.Models;
+using apointify.VirtualModels;
 using Microsoft.AspNetCore.Mvc;
 
 
@@ -22,15 +24,109 @@ namespace apointify.Controllers
         }
 
 
-        public IActionResult Create(FirmDetail firm) 
-        {
-
-
-
-            return RedirectToAction("Index","Login");
-        }
 
         
+
+
+        public IActionResult Create(FirmDetailVM firm) 
+        {
+            try
+            {
+                HttpContext.Session.Clear();
+                OmParmarContext DBEntities = new OmParmarContext();
+                if (firm.FirmId == null)
+                {
+
+
+                    
+
+                    if(firm.Image == null)
+                    {
+                        string path = "wwwroot/Image/FirmImage";
+
+                        //create folder if not exist
+                        if (!Directory.Exists(path))
+                            Directory.CreateDirectory(path);
+
+                        //get file extension
+                        FileInfo fileInfo = new FileInfo(firm.Image.FileName);
+                        string fileName = firm.Image.FileName;
+
+                        string fileNameWithPath = Path.Combine(path, fileName);
+                    }
+                    else
+                    {
+                        string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Image/FirmImage");
+
+                        //create folder if not exist
+                        if (!Directory.Exists(path))
+                            Directory.CreateDirectory(path);
+
+                        //get file extension
+                        FileInfo fileInfo = new FileInfo(firm.Image.FileName);
+                        string fileName = firm.Image.FileName;
+
+                        string fileNameWithPath = Path.Combine(path, fileName);
+
+                        using (var stream = new FileStream(fileNameWithPath, FileMode.Create))
+                        {
+                            firm.Image.CopyTo(stream);
+                        }
+                    }
+                    
+
+
+                    FirmDetail newfirm = firm.ToContext();
+                    firm.FirmId = new int();
+                    DBEntities.FirmDetails.Add(newfirm);
+                    DBEntities.SaveChanges();
+                    return RedirectToAction("Index", "Login");
+                }
+                else if (firm.FirmId != null)
+                {
+                    FirmDetail updatefirm = DBEntities.FirmDetails.Where(m => m.FirmId == firm.FirmId).FirstOrDefault();
+
+                    string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Image/FirmImage");
+
+                    //create folder if not exist
+                    if (!Directory.Exists(path))
+                        Directory.CreateDirectory(path);
+
+                    //get file extension
+                    FileInfo fileInfo = new FileInfo(firm.Image.FileName);
+                    string fileName = firm.Image.FileName;
+
+                    string fileNameWithPath = Path.Combine(path, fileName);
+
+
+                    using (var stream = new FileStream(fileNameWithPath, FileMode.Create))
+                    {
+                        firm.Image.CopyTo(stream);
+                    }
+
+                    updatefirm.FirmImage = fileNameWithPath;
+                    updatefirm.FirmName = firm.FirmName;
+                    updatefirm.MobileNumber = firm.FirmName;
+                    updatefirm.Address = firm.Address;
+                    updatefirm.Email = firm.Email;
+                    return RedirectToAction("Index", "Login");
+                }
+
+
+                return RedirectToAction("Index", "Login");
+
+
+
+            }
+            catch(Exception ex)
+            {
+                throw;
+            }
+            
+
+        }
+
+
 
     }
 }
--- apointify/VirtualModels/FirmDetailVM.cs
+++ apointify/VirtualModels/FirmDetailVM.cs
@@ -12,6 +12,7 @@
 
         public string FirmName { get; set; } = null!;
 
+        public IFormFile Image { get; set; } = null!;
         public string FirmImage { get; set; } = null!;
 
         public string Email { get; set; } = null!;
